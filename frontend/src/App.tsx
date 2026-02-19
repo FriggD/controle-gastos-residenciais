8de31a354730a8e5eb26fdcf5b80ecafdb216c88
Frigg <glaucia.belo.dias@gmail.com>
2026-02-19 08:53:21 -0300

    feat: add initial project structure with routing, styles, and configuration files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import MockDataButtons from './components/MockDataButtons';
+import PessoasPage from './pages/PessoasPage';
+import CategoriasPage from './pages/CategoriasPage';
+import TransacoesPage from './pages/TransacoesPage';
+import RelatoriosPage from './pages/RelatoriosPage';
+import './styles/global.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<PessoasPage />} />
+        <Route path="/categorias" element={<CategoriasPage />} />
+        <Route path="/transacoes" element={<TransacoesPage />} />
+        <Route path="/relatorios" element={<RelatoriosPage />} />
+      </Routes>
+      <MockDataButtons />
+    </BrowserRouter>
+  );
+}
+
+export default App;
